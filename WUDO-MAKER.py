d5fa2c4a798b7658b14fd6e6e04c95234fae6e1a
Tertle950 <tertle950@gmail.com>
2023-01-20 18:53:57 +0000

    D2 Midterm requirements fulfilled...not much else

--- WUDO-MAKER.py
+++ WUDO-MAKER.py
@@ -1,21 +1,92 @@
 import os
-#import pwd
+import pwd
 #import crypt
 #import getpass
 import subprocess
 
+def callExplicitly(command):
+    print("Calling",end="")
+    for i in command:
+        print(" "+i,end="")
+    print("...")
+
+    subprocess.call(command)
+
+#https://stackoverflow.com/questions/842059/is-there-a-portable-way-to-get-the-current-username-in-python
+def getUsername():
+    return pwd.getpwuid(os.getuid())[0]
+
+def createUser(userName, inWheelGroup):
+    print(" -= Here's the part where I'd make the user",userName,"=-")
+    print(" -= In wheel group:",inWheelGroup,"=-")
+
+def removeUserFromWheelGroup(userName):
+    print(" -= Here's the part where I'd remove",userName,"from the wheel group =-")
+
+def inputValidUser():
+    print("Maybe I'll add opinions to this later. If I have to.")
+    return input("Enter a valid username. I'll just trust you on it...\n(str)? ")
+
 while True:
     print(
         "Enter mode:\n"+
-        "1) Create \"work user\" for current user\n"+
-        "2) Initialize multiple users with work users (for loop)"
+        "1) Personal use. Create \"work user\" for current user\n"+
+        "2) Workplace use. Initialize multiple users with work users"
     )
     try:
-        choice = int(input("? "))
+        choice = int(input("(int)? "))
         if choice >= 1 and choice <= 2:
             break
     except ValueError:
         pass
     print("Please enter a valid choice.")
 
-subprocess.call(['sudo', 'fish'])
+
+if choice == 1:
+    createUser(getUsername() + "-worker", False)
+
+    while True:
+        try:
+            print("Do you wish to remove yourself from the wheel group?")
+            choice = input("If yes, root or worker access would require su.\n(y/N)? ").lower()[0]
+            
+            if choice == 'y' or choice == 'n':
+                break
+            else: print("Please enter a valid choice.")
+        except IndexError:
+            choice = 'n'
+            break
+    if choice == 'y':
+        removeUserFromWheelGroup(getUsername())
+
+elif choice == 2:
+    while True:
+        try:
+            amount = int(input("Enter number of users to make.\n(int)? "))
+            break
+        except ValueError:
+            print("Please enter an integer.")
+    
+    newUsers = []
+    for i in range(amount):
+        newUsers.append(inputValidUser())
+    
+    while True:
+        try:
+            print("Should these users be in the wheel group?")
+            choice = input("If no, root or worker access would require su.\n(y/N)? ").lower()[0]
+            
+            if choice == 'y' or choice == 'n':
+                break
+            else: print("Please enter a valid choice.")
+        except IndexError:
+            choice = 'n'
+            break
+    
+    for user in newUsers:
+        createUser(user, False)
+        createUser(user + "-worker", False)
+
+
+else:
+    print("How'd you get here?")
